b922059a8aa8c06cef61f23df8b4f01d26d6621d
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-01 01:42:13 +0100

    added path to views files

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    res.send('<body><h2> Hello from Express!!! </h2></body>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
