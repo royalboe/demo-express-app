73d65c9ff449a29033b9b937ce64d5193b6bdb42
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-01 01:12:57 +0100

    changed to add-product route but with different protocols

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 
 router.use('/add-product', (req, res, next) => {
-    res.send('<body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></body>');
+    res.send('<body><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></body>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
