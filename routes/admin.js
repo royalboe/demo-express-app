75b3fbd5b85444503c298cd7e9b895713dca42c0
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-06 00:25:56 +0100

    bug-fix, admin.add-products now routes to home

--- routes/admin.js
+++ routes/admin.js
@@ -7,11 +7,14 @@ const router = express.Router();
 // To store data
 const products = [];
 
-router.use('/add-product', (req, res, next) => {
+// /admin/add-product => GET Method
+router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/add-product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) =>
+{
     products.push({'title': req.body.title});
     res.redirect('/');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,9 @@ const rootDir = require('../util/path');
 
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // console.log('shop.js', adminData.products);
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'shop'});
 });
 
 module.exports = router;
