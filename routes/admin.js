fedf9c5dfda4ead380b5098d60ccf053aea8190e
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-06 07:22:36 +0100

    Revert "Passed in a json document to be rendered file"
    This reverts commit 0a95860c05f46d4f10633d80707ac248d9436f4d.

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const products = [];
 
 // /admin/add-product => GET Method
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { docTitle: 'Add Product' });
+    res.render('add-product');
 });
 
 // /admin/add-product => POST
