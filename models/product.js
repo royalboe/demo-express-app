e56266ac197bbb74f8d31777940b9d4252813f1a
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-11 06:50:19 +0100

    added an helper function

--- models/product.js
+++ models/product.js
@@ -4,43 +4,37 @@ const path = require('path');
 const rootDir = require("../util/path");
 
 const p = path.join(rootDir, 'data', 'products.json');
+
+// Helper function to help retrieve product from file
+const getProductsFromFIle = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent))
+        }
+    });
+}
+
 // Product model class
-module.exports = class Product
-{
+module.exports = class Product {
     // Constructor to instantiate products
-    constructor(t)
-    {
+    constructor(t) {
         this.title = t;
     }
 
     // Method to save the product
-    save()
-    {
-        fs.readFile(p, (err, fileContent) =>
-        {
-            let products = [];
-            if (!err)
-            {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) =>
-            {
-                console.log(err);
-            });
+    save() {
+        getProductsFromFIle((products) => {
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), (err) => {
+            console.log(err);
+          });
         });
     }
 
     // Static method to return products
-    static fetchAll(cb)
-    {
-        fs.readFile(p, (err, fileContent) =>
-        {
-            if (err)
-            {
-                return cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+    static fetchAll(cb) {
+        getProductsFromFIle(cb);
     }
 };
