9ed358da1a383f200b3c07c2756187da1a590bb2
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-11 06:50:50 +0100

    removed unused variables

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const path = require('path');
 
 const productController = require("../controllers/products");
 
 const router = express.Router();
-const rootDir = require('../util/path');
-
 
 router.get('/', productController.getProducts);
 
