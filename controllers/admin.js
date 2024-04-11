66582d689c00f42f333ef918ac2fa9e777e54e4b
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-11 14:25:30 +0100

    created the controllers to display views

---
+++ controllers/admin.js
@@ -0,0 +1,36 @@
+const Product = require("../models/product");
+
+// Renders the add product view to give product details
+exports.addProducts = (req, res, next) => {
+	res.render("admin/add-product", {
+		docTitle: "Add Product",
+		path: "/add-product",
+		formsCSS: true,
+		productCSS: true,
+		activeAddProduct: true,
+	});
+};
+
+// Controller method to get product details from view and make a product object and redirects to home
+exports.postProducts = (req, res, next) => {
+	const title = req.body.title;
+	const imageURL = req.body.imageURL;
+	const price = req.body.price;
+	const description = req.body.description;
+
+	const product = new Product(title, imageURL, price, description);
+	product.save();
+	res.redirect("/");
+};
+
+// Controller to render the admin product page
+exports.getProducts = (req, res, next) => {
+	Product.fetchAll((products) => {
+		res.render("admin/products", {
+			prods: products,
+			docTitle: "Admin Products",
+			path: "/admin/products",
+			hasProducts: products.length > 0,
+		});
+	});
+};
