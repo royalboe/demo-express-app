e9581e40eea4f5cc225d6bd1e01287701f95130b
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-11 04:35:07 +0100

    exporting the routes and removed product data

--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,10 @@ const rootDir = require('../util/path');
 const productController = require('../controllers/products');
 const router = express.Router();
 
-// To store data
-const products = [];
-
 // /admin/add-product => GET Method
 router.get('/add-product', productController.addProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', productController.postProducts);
 
-exports.router = router;
-exports.products = products;
+module.exports = router;
