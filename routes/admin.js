15f5a6a000512989fc939070c6029d8de5f5f262
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-11 14:10:29 +0100

    updated the admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
+const adminController = require('../controllers/products');
 
-const rootDir = require('../util/path');
-const productController = require('../controllers/products');
 const router = express.Router();
 
 // /admin/add-product => GET Method
-router.get('/add-product', productController.addProducts);
+router.get('/add-product', adminController.addProducts);
 
-// /admin/add-product => POST
-router.post('/add-product', productController.postProducts);
+// /admin/products => GET Method
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST Method
+router.post('/add-product', adminController.postProducts);
 
 module.exports = router;
