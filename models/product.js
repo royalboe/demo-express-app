351a25e76a4f8f34647b631c1e13e117c0ecec5d
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-20 11:26:16 +0100

    added features to save updated products

--- models/product.js
+++ models/product.js
@@ -19,18 +19,26 @@ const getProductsFromFIle = cb => {
 // Product model class
 module.exports = class Product {
 	// Constructor to instantiate products
-	constructor(title, imageURL, description, price) {
-        this.title = title;
-        this.imageURL = imageURL;
-        this.description = description;
-        this.price = price;
-	}
+	constructor(id, title, imageURL, description, price) {
+		this.id = id;
+		this.title = title;
+		this.imageURL = imageURL;
+		this.description = description;
+		this.price = price;
+  }
 
 	// Method to save the product
 	save() {
 		getProductsFromFIle((products) => {
-			this.id = Math.random().toString();
-			products.push(this);
+			if (this.id) {
+				const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+				const updatedProducts = [...products];
+				updatedProducts[existingProductIndex] = this;
+				products = updatedProducts;
+			} else {
+				this.id = Math.random().toString();
+				products.push(this);
+			}
 			fs.writeFile(p, JSON.stringify(products), (err) => {
 				console.log(err);
 			});
