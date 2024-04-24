29c13bf3db2604d80f4f2f83bc5570f548b9b325
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-04-24 22:17:44 +0100

    added users and user table

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/404')
 const sequelize = require('./util/database');
-
+const User = require('./models/users');
+const Product = require('./models/product');
 // const expressHbs = require("express-handlebars");
 
 const app = express();
@@ -34,15 +35,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 // This allows you to access form data submitted in POST requests via req.body in your route handlers
 app.use(bodyParser.urlencoded({extended: false}));
 
+// This is to register the user
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then(user => {
+			req.user = user;
+			next();
+		})
+		.catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
 app.use(errorController.error);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize.sync()
-  .then(result => {
-    console.log(`DB created`);
+	.then(() => {
+		return User.findByPk(1);
+	})
+	.then(user => {
+		if (!user) {
+			return User.create({ name: 'Max', email: 'test@test.com' });
+		}
+		return user;
+	})
+	.then(user => {
+		// console.log(user);
 		app.listen(3000);
 	})
 	.catch(err => console.log(err));
