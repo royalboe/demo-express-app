03a96096b5ca7e8a04b2b4ff7d326cc33a6d353b
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2024-11-06 17:11:30 +0100

    made minor adjustments

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,7 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
+	
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
@@ -270,8 +271,15 @@ exports.postResetPage = (req, res, next) => {
 
 exports.getNewPassword = (req, res, next) => {
 	const token = req.params.token;
-	User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+	User.findOne({
+		resetToken: token,
+		resetTokenExpiration: { $gt: Date.now() }
+	})
 		.then((user) => {
+			if (!user) {
+				req.flash("error", "No user found");
+				return res.redirect("/");
+			}
 			let errorMessage = req.flash("error");
 			if (errorMessage.length > 0) {
 				errorMessage = errorMessage[0];
